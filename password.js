82d7ee459c021d6f5733c14dc0c1ef72aebb9e23
Raro Macias <ggmacias@gmail.com>
2022-08-19 14:50:44 -0400

    password

---
+++ password.js
@@ -0,0 +1,16 @@
+//*Create a var called password and set it to a string of my choosing.
+//Create a conditional statement that checks if the password meets the following criteria;
+//At least 10 characters long
+//Contains at least one letter and one number
+//if it meets constraint console log a success message if it fails console log a failure message
+//test out the conditional by using different values for password.
+
+let password = "KobeBean24"
+console.log(password.length)
+    if(password.length >= 10){
+        console.log("Success!")
+    }else{
+        console.log("Failure!")
+    }
+
+    
