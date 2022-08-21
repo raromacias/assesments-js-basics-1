f83d6c7390d8d6d387ff9c489eb52e88ba5b23a2
Raro Macias <ggmacias@gmail.com>
2022-08-21 14:02:06 -0400

    Fixed code

--- password.js
+++ password.js
@@ -5,17 +5,28 @@
 //if it meets constraint console log a success message if it fails console log a failure message
 //test out the conditional by using different values for password.
 
-let password = "KobeBean24"
-let testArr = password.slice(0, password.length)
-console.log(testArr)
-console.log(password.length)
-    if(password.length >= 10){
-        console.log("Success!")
-    }else{
-        console.log("Failure!")
-    }
-    // for (i=0; i < password.length; i++){
-        
-    // }
+let password = "Kobebean24"
+let minChar = false
+let numCheck = false
+let letterCheck = false
 
-    
+    if(password.length >= 10){
+        minChar = true
+    }
+     for (let i=0; i < password.length; i++){
+        if(!isNaN(+password[i])){
+            numCheck = true
+        }
+        // if (password[i] === password[i].toUpperCase() && isNaN(+password[i])){
+        //     upperCheck = true
+        // }
+     //I ended up checking for both upper case and lower case letters
+        if(/[a-z]/i.test(password)){
+        letterCheck = true
+     }
+    }
+    if(minChar && letterCheck && numCheck){
+        console.log("Your password fills all the requirements")
+    } else {
+        console.log("The password does not fill all requirements")
+    }
